6e22f2a8cbc158f8d5c933810450b5b18161e6f2
Danny Rooker <dannyrooks@gmail.com>
2019-06-29 15:52:53 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-
+addition = 5 + 4  
 end
 
 def subtraction(num1, num2)
-
+subtraction = 10 - 5
 end
 
 def division(num1, num2)
-
+division = 50 / 2
 end
 
 def multiplication(num1, num2)
-
+multiplication = 4 * 30
 end
 
 def modulo(num1, num2)
-
+modulo = 34 % 5
 end
 
 def square_root(num)
-
+square_root = Integer.sqrt(81)
 end
